38a84e041a1da5212499e41572e915841d57cd86
John Jennings <john.jennings@lit.ie>
2019-02-28 15:49:39 +0000

    Added Student sites

---
+++ sites/site2/js/myScriptsBarrBr.js
@@ -0,0 +1,27 @@
+function swap(imgnum)
+{ 	if (imgnum==1) {
+		document.getElementById("pic").src = "img/barrfam02.PNG";
+		document.getElementById("pic").title = "Image 1: Barrington's Bridge - Bicentenary Celebration";
+	} else if (imgnum==2) {	
+		document.getElementById("pic").src = "img/barrbr01.PNG";
+		document.getElementById("pic").title = "Image 2: Barrington's Bridge";
+	} else if (imgnum==3) {	
+		document.getElementById("pic").src = "img/br05.PNG";
+		document.getElementById("pic").title = "Image 3: Bicentenary Birthday 1";
+	} else if (imgnum==4) {	
+		document.getElementById("pic").src = "img/bridge02.PNG";
+		document.getElementById("pic").title = "Image 4: Bicentenary Birthday 2";
+	} else if (imgnum==5) {	
+		document.getElementById("pic").src = "img/ br08.PNG";
+		document.getElementById("pic").title = "Image 5: Barrington Bridge- Side View";
+	} else if (imgnum==6) {	
+		document.getElementById("pic").src = "img/banner04.PNG";
+		document.getElementById("pic").title = "Image 6: Barrington Bridge Celebrations 1";
+	}	else if (imgnum==7) { 
+		document.getElementById("pic").src = "img/br011.PNG ";
+		document.getElementById("pic").title = "Image 7: Barrington Bridge Celebrations 2";
+	}	else if (imgnum==8) {	
+		document.getElementById("pic").src = "img/Cake01.PNG";
+		document.getElementById("pic").title = "Image 8: Barrington Cake Celebration";
+	}
+}
